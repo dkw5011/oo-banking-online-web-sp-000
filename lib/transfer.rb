e2f263fd273109dd5f7510c6155b34d6034314e6
Danielle Bergbower <dkw5011@gmail.com>
2019-11-24 23:30:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ def valid?
 end
 
 def execute_transaction
-  if @sender.balance > @amount && @status == "pending"
+  if @sender.balance > @amount && @status == "pending" && valid? == 'true'
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
